f44678758f582cecd777f1c1d7c8191cd01b9ed0
rupeshrautt@gmail.com <rupeshrautt@gmail.com>
2012-07-25 16:59:50 -0400

    initial check-in

---
+++ test/src/main/java/com/dev/test/Test.java
@@ -0,0 +1,18 @@
+package com.dev.test;
+
+/**
+ * The Class Test.
+ */
+public class Test {
+
+	/**
+	 * The main method.
+	 * 
+	 * @param args
+	 *            the arguments
+	 */
+	public static void main(String[] args) {
+
+	}
+
+}
