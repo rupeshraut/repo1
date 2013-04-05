85c5ce690d479cf08c661315b465d19a12437cbc
rupeshrautt@gmail.com <rupeshrautt@gmail.com>
2013-04-05 08:15:01 -0400

    added junit maven dependency and log4j.xml

---
+++ reporting-fwk/src/test/java/com/dev/reporting/test/Test.java
@@ -0,0 +1,26 @@
+package com.dev.reporting.test;
+
+import static org.junit.Assert.fail;
+
+/**
+ * The Class Test.
+ */
+public class Test {
+
+	/**
+	 * Test export export type string.
+	 */
+	@org.junit.Test
+	public void testExportExportTypeString() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test export export type.
+	 */
+	@org.junit.Test
+	public void testExportExportType() {
+		fail("Not yet implemented");
+	}
+
+}
