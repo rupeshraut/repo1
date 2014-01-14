18d9794692a0d85ea7c3143849b0b5d8ee40a1c3
rupeshraut <rupeshrautt@gmail.com>
2014-01-14 03:16:56 -0500

    RR - paginator implemented

--- jsp-tags/src/main/java/org/jtags/web/servlet/ListServlet.java
+++ jsp-tags/src/main/java/org/jtags/web/servlet/ListServlet.java
@@ -9,8 +9,12 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-// TODO: Auto-generated Javadoc
+import org.apache.commons.lang.StringUtils;
+import org.jtags.datatable.DataPaginator;
+import org.jtags.datatable.DataPaginatorException;
+
 /**
  * Servlet implementation class ListServlet.
  */
@@ -40,10 +44,10 @@ public class ListServlet extends HttpServlet {
 	 *             the servlet exception
 	 * @throws IOException
 	 *             Signals that an I/O exception has occurred.
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-	 *      response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println(request.getQueryString());
 		doPost(request, response);
 	}
 
@@ -58,17 +62,74 @@ public class ListServlet extends HttpServlet {
 	 *             the servlet exception
 	 * @throws IOException
 	 *             Signals that an I/O exception has occurred.
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
-	 *      response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		List<Items> items = new ArrayList<>();
-		Random random = new Random();
-		for (int i = 1; i <= 100; i++) {
-			items.add(new Items("item#"+i, random.nextDouble()));
+		try {
+			final HttpSession session = request.getSession(true);
+			@SuppressWarnings("unchecked")
+			DataPaginator<Items> dataPaginator = (DataPaginator<Items>) session.getAttribute("SESSION_ITEMS");
+			final String page = (String) request.getParameter("page");
+			
+			if (StringUtils.isEmpty(page)) {
+				dataPaginator = null;
+				session.removeAttribute("SESSION_ITEMS");
+			}
+			
+			if (dataPaginator != null) {
+				if (StringUtils.isNotEmpty(page)) {
+					if (StringUtils.equalsIgnoreCase(page, "first")) {
+						final List list = dataPaginator.first();
+						request.setAttribute("items", list);
+						request.setAttribute("from", 1);
+						request.setAttribute("to",  (list.size() % dataPaginator.getRowsPerPage()) == 0 ? dataPaginator.getCurrentPageIndex() * dataPaginator.getRowsPerPage() : (dataPaginator.getSize() % dataPaginator.getRowsPerPage()));
+						request.setAttribute("currentPage", dataPaginator.getCurrentPageIndex());
+					} else if (StringUtils.equalsIgnoreCase(page, "next")) {
+						final List list = dataPaginator.next();
+						request.setAttribute("items", list);
+						request.setAttribute("currentPage", dataPaginator.getCurrentPageIndex());
+						request.setAttribute("from", ( dataPaginator.getCurrentPageIndex() * dataPaginator.getRowsPerPage()) - dataPaginator.getRowsPerPage()+1);
+						request.setAttribute("to",  (list.size() % dataPaginator.getRowsPerPage()) == 0 ? dataPaginator.getCurrentPageIndex() * dataPaginator.getRowsPerPage() : dataPaginator.getSize());						
+					} else if (StringUtils.equalsIgnoreCase(page, "previous")) {
+						final List list = dataPaginator.previous();
+						request.setAttribute("items", list);
+						request.setAttribute("currentPage", dataPaginator.getCurrentPageIndex());
+						request.setAttribute("from", ( dataPaginator.getCurrentPageIndex() * dataPaginator.getRowsPerPage()) - dataPaginator.getRowsPerPage() + 1);
+						request.setAttribute("to",  (list.size() % dataPaginator.getRowsPerPage()) == 0 ? dataPaginator.getCurrentPageIndex() * dataPaginator.getRowsPerPage() : (dataPaginator.getSize() % dataPaginator.getRowsPerPage()));
+					} else if (StringUtils.equalsIgnoreCase(page, "last")) {
+						final List list = dataPaginator.last();
+						request.setAttribute("items", dataPaginator.last());
+						request.setAttribute("currentPage", dataPaginator.getCurrentPageIndex());
+						request.setAttribute("from", ( dataPaginator.getCurrentPageIndex() * dataPaginator.getRowsPerPage()) - dataPaginator.getRowsPerPage()+1);
+						request.setAttribute("to",  (list.size() % dataPaginator.getRowsPerPage()) == 0 ? dataPaginator.getCurrentPageIndex() * dataPaginator.getRowsPerPage() : dataPaginator.getSize());
+					}// if-else-if
+				}// if
+
+				request.setAttribute("totalItems", dataPaginator.getSize());
+				request.setAttribute("totalPages", dataPaginator.getNumberOfPages());
+				
+			} else {
+				List<Items> items = new ArrayList<>();
+				Random random = new Random();
+				for (int i = 1; i <= 20000; i++) {
+					items.add(new Items("item#" + i, random.nextDouble()));
+				}// for
+				dataPaginator = new DataPaginator<>(items);
+				dataPaginator.setRowsPerPage(20);
+				session.setAttribute("SESSION_ITEMS", dataPaginator);
+				request.setAttribute("items", dataPaginator.first());
+				request.setAttribute("currentPage", dataPaginator.getCurrentPageIndex());
+				request.setAttribute("totalItems", dataPaginator.getSize());
+				request.setAttribute("totalPages", dataPaginator.getNumberOfPages());
+				request.setAttribute("from", 1);
+				request.setAttribute("to",  (dataPaginator.getSize() % dataPaginator.getRowsPerPage()) == 0 ? dataPaginator.getCurrentPageIndex() * dataPaginator.getRowsPerPage() : (dataPaginator.getSize() % dataPaginator.getRowsPerPage()));
+			}// if-else
+
+			request.getRequestDispatcher("/WEB-INF/views/datatable.jsp").forward(request, response);
+
+		} catch (DataPaginatorException e) {
+			throw new ServletException(e.getMessage(), e);
 		}
-		request.setAttribute("items", items);
-		request.getRequestDispatcher("/WEB-INF/views/datatable.jsp").forward(request, response);
 	}
 
 }
